596282425ad841a329fd9424d3801e89d9d7652d
Bastien <bastien.brisson@etudiant.univ-rennes1.fr>
2023-03-12 22:55:26 +0100

    Implémentation des méthodes et début des tests

--- code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
@@ -1,11 +1,76 @@
 package fr.istic.vv;
 
 public class RomanNumeraUtils {
-    
-        public static boolean isValidRomanNumeral(String value) { return false; }
-    
-        public static int parseRomanNumeral(String numeral) { return 0; }
-    
-        public static String toRomanNumeral(int number) { return ""; }
+
+        private static final Map<Character, Integer> ROMAN_NUMERAL_VALUES = new HashMap<Character, Integer>() {{
+                put('I', 1);
+                put('V', 5);
+                put('X', 10);
+                put('L', 50);
+                put('C', 100);
+                put('D', 500);
+                put('M', 1000);
+        }};
+
+        /**
+         * Verifie si les values sont bien dans la liste pré-définie
+         * @param value
+         * @return
+         */
+        public static boolean isValidRomanNumeral(String value) {
+                if (value == null || value.isEmpty()) {
+                        return false;
+                }
+                for (char c : value.toCharArray()) {
+                        if (!ROMAN_NUMERAL_VALUES.containsKey(c)) {
+                                return false;
+                        }
+                }
+                return true;
+        }
+
+        /**
+         * Calcul la suite de caractere. Vérifie avant s'il est valide. Et fait le calcul. S'il est situé avant un nombre avant alors on soustrait et inversement
+         * @param numeral
+         * @return
+         */
+        public static int parseRomanNumeral(String numeral) {
+                if (!isValidRomanNumeral(numeral)) {
+                        throw new IllegalArgumentException("Invalid Roman numeral: " + numeral);
+                }
+                int result = 0;
+                int lastValue = 0;
+                for (int i = numeral.length() - 1; i >= 0; i--) {
+                        int currentValue = ROMAN_NUMERAL_VALUES.get(numeral.charAt(i));
+                        if (currentValue >= lastValue) {
+                                result += currentValue;
+                        } else {
+                                result -= currentValue;
+                        }
+                        lastValue = currentValue;
+                }
+                return result;
+        }
+
+        /**
+         *
+         * @param number
+         * @return
+         */
+        public static String toRomanNumeral(int number) {
+                if (number < 1 || number > 3999) {
+                        throw new IllegalArgumentException("Number out of range: " + number);
+                }
+                StringBuilder result = new StringBuilder();
+                int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+                String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+                for (int i = 0; i < values.length; i++) {
+                        while (number >= values[i]) {
+                                result.append(symbols[i]);
+                                number -= values[i];
+                        }
+                }
+                return result.toString();
+        }
     
 }
--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -1,9 +1,31 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+
 
 public class RomanNumeralTest {
     @Property
     boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
         return Math.abs(anInteger) >= 0;
+    }
+
+    @Property
+    void toRomanNumeralShouldProduceValidRomanNumeral(@ForAll @IntRange(min=0, max=3999) int number) {
+        String romanNumeral = RomanNumeraUtils.toRomanNumeral(number);
+        assertThat(RomanNumeraUtils.isValidRomanNumeral(romanNumeral)).isTrue();
+    }
+
+    @Property
+    void parseRomanNumeralShouldInverseToRomanNumeral(@ForAll("romanNumeral") String romanNumeral) {
+        int number = RomanNumeraUtils.parseRomanNumeral(romanNumeral);
+        assertThat(RomanNumeraUtils.toRomanNumeral(number)).isEqualTo(romanNumeral);
+    }
+
+    @Provide
+    Arbitrary<String> romanNumeral() {
+        return Arbitraries.strings()
+                .withCharRange('I', 'I').andChars('V', 'X', 'L', 'C', 'D', 'M')
+                .ofMinLength(1).ofMaxLength(15)
+                .injectDuplicates(0.2);
     }
 }
