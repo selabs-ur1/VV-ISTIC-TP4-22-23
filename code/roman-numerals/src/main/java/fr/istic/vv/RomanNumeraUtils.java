1dfb7e41b8cc09f4b6f9bd4df320262b192a1728
Ryan <ryan.yuechun@etudiant.univ-rennes1.fr>
2023-03-15 23:27:43 +0100

    Roman Numerals 2/3 done

--- code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
@@ -1,11 +1,120 @@
 package fr.istic.vv;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class RomanNumeraUtils {
+        private static final List<Character> triplableCharacters = new ArrayList<>(Arrays.asList('M', 'C', 'X', 'I'));
+        private static final List<Character> nonRepeatableCharacters = new ArrayList<>(Arrays.asList('D', 'L', 'V'));
+        private static final Map<Character, Integer> romanValues = Map.of(
+                'M', 1000,
+                'D', 500,
+                'C', 100,
+                'L', 50,
+                'X', 10,
+                'V', 5,
+                'I', 1
+        );
+
     
-        public static boolean isValidRomanNumeral(String value) { return false; }
-    
-        public static int parseRomanNumeral(String numeral) { return 0; }
-    
+        public static boolean isValidRomanNumeral(String value) {
+                for (char c : value.toCharArray()) {
+                        boolean invalidCharFound = !romanValues.containsKey(c);
+                        if (invalidCharFound) {
+                                return false;
+                        }
+                }
+
+                for (char c : triplableCharacters) {
+                        String quadCs = "" + c + c + c + c;
+                        boolean quadFound = value.contains(quadCs);
+
+                        if (quadFound) {
+                                return false;
+                        }
+                }
+
+                for (char c : nonRepeatableCharacters) {
+                        String doubleCs = "" + c + c;
+                        boolean doubleFound = value.contains(doubleCs);
+
+                        if (doubleFound) {
+                                return false;
+                        }
+                }
+
+                return true;
+        }
+
+
+        public static int parseRomanNumeral(String numeral) {
+                List<Integer> numbers = new ArrayList<>();
+                char[] inputs = numeral.toCharArray();
+
+                for (Character c : inputs) {
+                        numbers.add(romanValues.get(c));
+                }
+
+                List<Integer> sameNumbers1 = new ArrayList<>();
+                List<Integer> sameNumbers2 = new ArrayList<>();
+                Integer result = 0;
+                Collections.reverse(numbers);
+
+                for (Integer input : numbers) {
+                        result = oneCycle(sameNumbers1, sameNumbers2, result, input);
+                }
+                result = oneCycle(sameNumbers1, sameNumbers2, result, 0);
+
+                return oneCycle(sameNumbers1, sameNumbers2, result, 0);
+        }
+
+        private static Integer oneCycle(List<Integer> sameNumbers1, List<Integer> sameNumbers2, Integer result, Integer input) {
+                if (sameNumbers2.size() < 1) {
+                        if (sameNumbers1.size() > 0) {
+                                // fill sameNumbers1 or prepare next operation
+                                final int values1 = sameNumbers1.get(0);
+
+                                if (input == values1) {
+                                        sameNumbers1.add(input);
+                                        return result;
+                                }
+                                // else act on sameNumbers2
+                        } else {
+                                sameNumbers1.add(input);
+                                return result;
+                        }
+                }
+
+                // acting on sameNumbers2
+                if (sameNumbers2.size() == 0) {
+                        sameNumbers2.add(input);
+                } else {
+                        final int values2 = sameNumbers2.get(0);
+
+                        if (input == values2) {
+                                sameNumbers2.add(input);
+                        } else {
+                                // do operation
+                                final int values1 = sameNumbers1.get(0);
+                                final int sum1 = sameNumbers1.stream().mapToInt(Integer::intValue).sum();
+                                final int sum2 = sameNumbers2.stream().mapToInt(Integer::intValue).sum();
+
+                                if (values2 > values1) {
+                                        result = result + sum2 + sum1;
+                                } else if (values2 < values1) {
+                                        result = result + sum1 - sum2;
+                                }
+
+                                // reset lists, then add next input
+                                sameNumbers1.clear();
+                                sameNumbers2.clear();
+                                sameNumbers1.add(input);
+                        }
+                }
+                return result;
+        }
+
         public static String toRomanNumeral(int number) { return ""; }
     
 }
--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -1,9 +1,96 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.CharsList;
+import net.jqwik.api.constraints.IntRange;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class RomanNumeralTest {
+    private static final List<Character> allowedCharacters = new ArrayList<>(Arrays.asList('M', 'D', 'C', 'L','X', 'V', 'I'));
+    private static final List<Character> triplableCharacters = new ArrayList<>(Arrays.asList('M', 'C', 'X', 'I'));
+    private static final List<Character> nonRepeatableCharacters = new ArrayList<>(Arrays.asList('D', 'L', 'V'));
+    private static final Map<Character, Integer> romanValues = Map.of(
+            'M', 1000,
+            'D', 500,
+            'C', 100,
+            'L', 50,
+            'X', 10,
+            'V', 5,
+            'I', 1
+    );
+
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
+    boolean absoluteValueOfAllNumbersIsPositive(@ForAll @IntRange int anInteger) {
         return Math.abs(anInteger) >= 0;
     }
+
+    @Property
+    boolean checkCharactersOfRomanNumeral(@ForAll("invalidChars") char invalidChar) {
+        String invalidInput = "M" + invalidChar + "M";
+        boolean invalidCharFound = !RomanNumeraUtils.isValidRomanNumeral(invalidInput);
+        return invalidCharFound;
+    }
+
+    @Property
+    boolean checkTripledCharactersOfRomanNumeral(@ForAll("quadChars") String quadChar) {
+        boolean overTripleFound = !RomanNumeraUtils.isValidRomanNumeral(quadChar);
+        return overTripleFound;
+    }
+
+    @Property
+    boolean checkUnrepeatableCharactersOfRomanNumeral(@ForAll("repeatOfUnrepeatableChars") String repeatedChar) {
+        boolean unrepeatableFound = !RomanNumeraUtils.isValidRomanNumeral(repeatedChar);
+        return unrepeatableFound;
+    }
+
+    @Property
+    boolean checkLeftHighRightLow(@ForAll("leftHighRightLow") String highLow) {
+        return RomanNumeraUtils.parseRomanNumeral(highLow) > romanValues.get(highLow.charAt(0));
+    }
+
+    @Property
+    boolean checkLeftLowRightHigh(@ForAll("leftLowRightHigh") String lowHigh) {
+        return RomanNumeraUtils.parseRomanNumeral(lowHigh) < romanValues.get(lowHigh.charAt(1));
+    }
+
+    @Provide
+    Arbitrary<Character> invalidChars() {
+        return Arbitraries.chars().filter(c -> !allowedCharacters.contains(c));
+    }
+
+    @Provide
+    Arbitrary<String> quadChars() {
+        return Arbitraries.of(triplableCharacters).map(c -> "" + c + c + c + c);
+    }
+
+    @Provide
+    Arbitrary<String> repeatOfUnrepeatableChars() {
+        return Arbitraries.of(nonRepeatableCharacters).map(c -> "" + c + c);
+    }
+
+    @Provide
+    Arbitrary<String> leftHighRightLow() {
+        List<Character> lows = new ArrayList<>(allowedCharacters);
+        lows.remove(0);
+
+        Character low = Arbitraries.of(lows).sample();
+        List<Character> highChars = allowedCharacters.subList(0, allowedCharacters.indexOf(low));
+        return Arbitraries.of(highChars).map(high -> "" + high + low);
+    }
+
+    @Provide
+    Arbitrary<String> leftLowRightHigh() {
+        List<Character> highs = new ArrayList<>(allowedCharacters);
+        highs.remove(highs.size() - 1);
+
+        Character high = Arbitraries.of(highs).sample();
+        List<Character> lowChars = allowedCharacters.subList(
+                allowedCharacters.indexOf(high) + 1, allowedCharacters.size() - 1);
+        return Arbitraries.of(lowChars).map(low -> "" + low + high);
+    }
+
 }
