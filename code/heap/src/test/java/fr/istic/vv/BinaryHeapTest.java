8757074cdadb75a218945127e328e3adea562657
hwang <>
2023-03-19 00:36:38 +0100

    Finished heap implementation

--- code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
+++ code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
@@ -1,10 +1,60 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+import net.jqwik.api.constraints.Size;
+import org.assertj.core.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 
 
 public class BinaryHeapTest {
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    void containsSameElementsInAnyOrderInSource(@ForAll @Size(min = 1, max = 20) List<@IntRange(min = -114, max = 514) Integer> input) {
+        Comparator<Integer> order = Comparator.naturalOrder();
+
+        BinaryHeap<Integer> heap = new BinaryHeap<>(order);
+
+        List<Integer> stack = new ArrayList<>();
+
+        var rnd = new Random();
+
+        for (var v : input) {
+            heap.push(v);
+            if (rnd.nextBoolean()) {
+                stack.add(heap.pop());
+            }
+        }
+
+        while (heap.count() > 0) {
+            stack.add(heap.pop());
+        }
+
+        Assertions.assertThat(stack)
+                .containsExactlyInAnyOrderElementsOf(input);
+    }
+
+    @Property
+    void ascendingOrderForMinimumHeap(@ForAll @Size(min = 1, max = 20) List<@IntRange(min = -114, max = 514) Integer> input) {
+        Comparator<Integer> order = Comparator.naturalOrder();
+
+        BinaryHeap<Integer> heap = new BinaryHeap<>(order);
+
+        List<Integer> stack = new ArrayList<>();
+
+        for (var v : input) {
+            heap.push(v);
+        }
+
+        while (heap.count() > 0) {
+            stack.add(heap.pop());
+        }
+
+        Assertions.assertThat(stack)
+                .containsExactlyInAnyOrderElementsOf(input)
+                .isSortedAccordingTo(order);
     }
 }
