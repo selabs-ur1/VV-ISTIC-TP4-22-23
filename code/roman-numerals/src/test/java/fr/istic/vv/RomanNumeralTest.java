ee11ae72a5912d130d40092c4b4d38a926654cb6
eremus <vincent.amiot@etudiant.univ-rennes1.fr>
2023-03-18 03:26:54 +0100

    rendu

--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -1,9 +1,32 @@
 package fr.istic.vv;
+
 import net.jqwik.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RomanNumeralTest {
+
+    /**
+     * This test is nor doable because the coding this test would be the same that the function
+     * @param input
+     * @return
+     */
+    /*
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean testIsValidRomanNumeral(@ForAll String input) {
+        return RomanNumeraUtils.isValidRomanNumeral(input);
+    }
+    */
+
+    @Property
+    boolean testParseRomanNumeral(@ForAll int input) {
+        Assume.that(input >= 1 && input <= 3999); // add this assumption to limit the input range
+        return input == RomanNumeraUtils.parseRomanNumeral(RomanNumeraUtils.toRomanNumeral(input));
+    }
+
+    @Property
+    boolean testToRomanNumeral(@ForAll int input) {
+        Assume.that(input >= 1 && input <= 3999); // add this assumption to limit the input range
+        String romanNumeral = RomanNumeraUtils.toRomanNumeral(input);
+        return input == RomanNumeraUtils.parseRomanNumeral(romanNumeral) && RomanNumeraUtils.isValidRomanNumeral(romanNumeral);
     }
 }
