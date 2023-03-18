a7e5ae6a7a1c0cb614923109bcb777dd9e9550d0
hwang <>
2023-03-18 18:28:16 +0000

    Implemented three algorithms

--- code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,86 @@
 package fr.istic.vv;
 
-import java.util.Comparator;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BinaryHeap<T> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    private List<T> array;
 
-    public T pop() { return null; }
+    private Comparator<T> comparator;
 
-    public T peek() { return null; }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.array = new ArrayList<>();
+        this.comparator = comparator;
+    }
 
-    public void push(T element) { }
+    private static int parent(int index) {
+        return (index - 1) / 2;
+    }
 
-    public int count() { return 0; }
+    private static int leftSubNode(int index) {
+        return index * 2 + 1;
+    }
+
+    private static int rightSubNode(int index) {
+        return (index + 1) * 2;
+    }
+
+    private void swap(int posa, int posb) {
+        T value = array.get(posa);
+        array.set(posa, array.get(posb));
+        array.set(posb, value);
+    }
+
+    private void liftUp() {
+        int index = this.array.size() - 1;
+        for (;index > 0; index = parent(index)) {
+            int parent = parent(index);
+            if (comparator.compare(array.get(parent), array.get(index)) > 0) {
+                swap(index, parent);
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void sinkDown(int index) {
+        int left = leftSubNode(index);
+        int right = rightSubNode(index);
+        int selected = index;
+        if (left < array.size() &&
+                comparator.compare(array.get(left), array.get(selected)) < 0) {
+            selected = left;
+        }
+        if (right < array.size() &&
+                comparator.compare(array.get(right), array.get(selected)) < 0) {
+            selected = right;
+        }
+        if (selected != index) {
+            swap(index, selected);
+            sinkDown(selected);
+        }
+    }
+
+    public T pop() {
+        T value = array.get(0);
+        swap(0, array.size() - 1);
+        array.remove(array.size() - 1);
+        sinkDown(0);
+        return value;
+    }
+
+    public T peek() { return array.get(0); }
+
+    public void push(T element) {
+        this.array.add(element);
+        liftUp();
+    }
+
+    public int count() { return array.size(); }
+
+    @Override public String toString() {
+        return String.join("", array.stream().map(Object::toString).collect(Collectors.joining(",")));
+    }
 
 }
--- code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
@@ -1,11 +1,70 @@
 package fr.istic.vv;
 
+import java.util.Map;
+
 public class RomanNumeraUtils {
-    
-        public static boolean isValidRomanNumeral(String value) { return false; }
-    
-        public static int parseRomanNumeral(String numeral) { return 0; }
-    
-        public static String toRomanNumeral(int number) { return ""; }
-    
+
+    private static Map<Character, Integer> symbols = Map.of(
+            'M', 1000,
+            'D', 500,
+            'C', 100,
+            'L', 50,
+            'X', 10,
+            'V', 5,
+            'I', 1);
+
+    private static String[] tpos3 = { "", "M", "MM", "MMM" };
+    private static String[] tpos2 = { "",  "C",  "CC",  "CCC",  "CD",
+            "D", "DC", "DCC", "DCCC", "CM" };
+    private static String[] tpos1 = { "",  "X",  "XX",  "XXX",  "XL",
+            "L", "LX", "LXX", "LXXX", "XC" };
+    private static String[] tpos0 = { "",  "I",  "II",  "III",  "IV",
+            "V", "VI", "VII", "VIII", "IX" };
+
+    public static boolean isValidRomanNumeral(String value) {
+        for (var i = 0; i < value.length(); i++) {
+            if ("MDCLXVI".contains(""+value.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int parseRomanNumeral(String numeral) {
+        var result = 0;
+        for (var i = 0; i < numeral.length(); i++) {
+            var current = symbols.get(numeral.charAt(i));
+            int next;
+            if (i + 1 < numeral.length()) {
+                next = symbols.get(numeral.charAt(i+1));
+            } else {
+                next = 0;
+            }
+
+            if (current < next) {
+                result += next - current;
+            } else {
+                result += current;
+            }
+        }
+        if (result > 0) {
+            return result;
+        } else {
+            throw new IllegalStateException("Not a valid number");
+        }
+
+    }
+
+    public static String toRomanNumeral(int number) {
+        var pos3 = tpos3[number / 1000];
+        number %= 1000;
+        var pos2 = tpos2[number / 100];
+        number %= 100;
+        var pos1 = tpos1[number / 10];
+        number %= 10;
+        var pos0 = tpos0[number % 10];
+
+        return "" + pos3 + pos2 + pos1 + pos0;
+    }
+
 }
