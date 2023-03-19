5455bac403c373b1577c023e6a9b893964b577a3
Ryan <ryan.yuechun@etudiant.univ-rennes1.fr>
2023-03-19 19:14:29 +0100

    Part 3 : heap DONE

--- code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
+++ code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
@@ -1,10 +1,40 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+
+import java.util.*;
 
 
 public class BinaryHeapTest {
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
+    boolean absoluteValueOfAllNumbersIsPositive(@ForAll @IntRange int anInteger) {
         return Math.abs(anInteger) >= 0;
+    }
+
+    @Property
+    boolean differentialHeapStream(@ForAll("arbitraryOfIntegers") List<Integer> listIntegers) {
+        Comparator<Integer> integerComparator = Comparator.naturalOrder();
+        BinaryHeap<Integer> heap = new BinaryHeap<>(integerComparator);
+
+        listIntegers.forEach(heap::push);
+        Optional<Integer> optionalMin = listIntegers.stream().min(integerComparator);
+
+        if (optionalMin.isPresent()) {
+            return Objects.equals(heap.pop(), optionalMin.get());
+        } else {
+            boolean noSuchElementExceptionFound;
+            try {
+                heap.pop();
+                noSuchElementExceptionFound = false;
+            } catch (NoSuchElementException noSuchElementException) {
+                noSuchElementExceptionFound = true;
+            }
+            return noSuchElementExceptionFound;
+        }
+    }
+
+    @Provide
+    Arbitrary<List<Integer>> arbitraryOfIntegers() {
+        return Arbitraries.integers().list();
     }
 }
